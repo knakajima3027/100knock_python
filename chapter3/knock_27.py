ac31246168f8a0f63c5ccf2e7d30919cc5fb0109
kNakajima <kamihate1012@gmail.com>
2018-08-07 22:49:51 +0900

    27追加

---
+++ chapter3/knock_27.py
@@ -0,0 +1,50 @@
+import re
+from knock_20 import pick_UK
+
+"""メモ
+(?: キャプチャ対象外にする
+"""
+
+UK_text = pick_UK()
+
+def cut_mark(mark_dict): #強調をカット
+    regex = r'\'{2,5}'
+    pattern = re.compile(regex)
+    for key in mark_dict.keys():
+        mark_dict[key] = pattern.sub('', mark_dict[key])
+    return mark_dict
+
+def cut_url_mark(mark_dict): #urlをカット
+    regex = r'''
+            \[\[
+            (?:
+                [^|]*?
+                \|
+            )?
+            ([^|]*?)
+            \]\]
+            '''
+    pattern = re.compile(regex, re.VERBOSE)
+    for key in mark_dict.keys():
+        mark_dict[key] = pattern.sub('', mark_dict[key])
+    return mark_dict
+
+regex1 = r'''\{\{基礎情報.*?$
+             (.*?)
+             ^\}\}$'''
+pattern1 = re.compile(regex1, re.MULTILINE + re.VERBOSE + re.DOTALL)
+lines1 = pattern1.findall(UK_text)
+
+regex2 = r'\|(.*?)\s*=\s*(.*)'
+pattern2 = re.compile(regex2)
+lines2 = pattern2.findall(lines1[0])
+
+
+uk_dict = {}
+for line in lines2:
+    uk_dict[line[0]] = line[1]
+
+result1 = cut_mark(uk_dict)
+result2 = cut_url_mark(result1)
+
+print(result2)
