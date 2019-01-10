08c9dadf5d83af3bcd227c2147954411d8f8e8c0
kNakajima3027 <kamihate1012@gmail.com>
2019-01-10 19:15:13 +0900

    修正

--- chapter3/knock_25.py
+++ chapter3/knock_25.py
@@ -6,20 +6,23 @@ re.MULTILINE 行毎にマッチさせる
 re.VERBOSE　正規表現の途中にコメントを入れられる
 re.DOTALL　. を改行にもマッチさせる
 '''
-UK_text = pick_UK()
+def make_dict(uk_dict):
+    UK_text = pick_UK()
 
-regex1 = r'''\{\{基礎情報.*?$
-             (.*?)
-             ^\}\}$'''
-pattern1 = re.compile(regex1, re.MULTILINE + re.VERBOSE + re.DOTALL)
-lines1 = pattern1.findall(UK_text)
+    regex1 = r'''\{\{基礎情報.*?$
+                 (.*?)
+                 ^\}\}$'''
+    pattern1 = re.compile(regex1, re.MULTILINE + re.VERBOSE + re.DOTALL)
+    lines1 = pattern1.findall(UK_text)
 
-regex2 = r'\|(.*?)\s*=\s*(.*)'
-pattern2 = re.compile(regex2)
-lines2 = pattern2.findall(lines1[0])
+    regex2 = r'\|(.*?)\s*=\s*(.*)'
+    pattern2 = re.compile(regex2)
+    lines2 = pattern2.findall(lines1[0])
 
-uk_dict = {}
-for line in lines2:
-    uk_dict[line[0]] = line[1]
-    
-print(uk_dict)
+    for line in lines2:
+        uk_dict[line[0]] = line[1]
+
+if __name__ == '__main__':
+    uk_dict = {}
+    make_dict(uk_dict)
+    print(uk_dict)
